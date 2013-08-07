3c4e1e7cac78f0e6f03d2d3a1db713338cb5e14c
James <james.naadjie@gmail.com>
2013-08-07 00:17:32 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nwokoroassociates::Application.config.secret_token = 'a119eff2df914176a39f3a5cceb5cd51b1b83a526630ac595b5d4f77e27a910fbc85320b73f09518e3f7eaff073eaf8b1392007bc9ae18853872ee33ed274c3d'
