43adb2785ebaa7fa144670ccdad41fd6d06fed59
James <james.naadjie@gmail.com>
2013-08-13 16:00:53 -0400

    associates form fix

--- app/admin/attorneys.rb
+++ app/admin/attorneys.rb
@@ -4,7 +4,7 @@ index do
     column :last_name        
     column :profile          
     column :avatar do |p|
-    	image_tag p.avatar.url
+    	image_tag p.avatar.url(:medium)
     end           
     default_actions                   
   end 
@@ -15,11 +15,18 @@ index do
    		row :last_name
    		row	:profile
    		row	:avatar do |p|
-   			image_tag p.avatar.url
+   			image_tag p.avatar.url(:medium)
    		end
        end
    end   
 
-  form	:partial => "form"
-
+   form do |f|
+    f.inputs do 
+    f.input :first_name
+    f.input :last_name
+    f.input :profile, :label => "Attorney Profile"
+    f.input :avatar, :label => "Profile Photo"
+   end
+     f.actions
+   end
 end
