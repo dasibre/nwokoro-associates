330bbf9af30a40d2b61b50b04854b21c24be70da
James Naadjie <james.naadjie@gmail.com>
2014-12-12 20:10:05 -0500

    Clean up files

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,16 @@
 Nwokoroassociates::Application.routes.draw do
 
- match 'contact', to: 'contact#new', as: 'contact', via: :get
- match 'contact', to: 'contact#create', as: 'contact', via: :post
-
+  resources :attorneys, only: [:index, :show]
   root :to => 'pages#home'
-  
-  # get "/news/posts", to: "blogit/posts#index"
 
-  get 'areas_of_practice', to: 'pages#areas_of_practice'
-  get 'contact_us', to: 'pages#contact_us'
-  get 'about_us', to: 'pages#about_us'
+  match 'contact', to: 'contact#new', as: 'contact', via: :get
+  match 'contact', to: 'contact#create', as: 'contact', via: :post
+
+
+  match 'areas_of_practice', to: 'pages#areas_of_practice'
+  match 'contact_us', to: 'pages#contact_us'
+  match 'about_us', to: 'pages#about_us'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   mount Blogit::Engine => '/news', as: 'news_blog'
-  
- resources :attorneys, only: [:index, :show]
-
 end
