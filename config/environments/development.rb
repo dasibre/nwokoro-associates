7340d3544db39204d6e21db1dd0e89d0b3207cf9
James Naadjie <james.naadjie@gmail.com>
2016-06-28 19:43:37 -0400

    add environment variable for paperclip

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -7,7 +7,7 @@ Nwokoroassociates::Application.configure do
 
   config.paperclip_defaults = {
       :storage => :s3,
-      :bucket => ENV['S3_BUCKET'],
+      :bucket => ENV['S3_BUCKET'] || 'Nwokoro',
       :s3_credentials => {
           :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
           :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
